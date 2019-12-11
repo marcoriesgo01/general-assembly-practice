80c9308acc631c189c643dab3bf6196655beac8c
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro.local>
2019-12-11 14:08:18 -0800

    finished loop practice

---
+++ week1_homework_Github_Practice_and_Resume_Page/loops.js
@@ -0,0 +1,20 @@
+//1.
+for (var i = 0; i <= 99; i++) {
+    console.log(i); 
+}
+//2.
+for (var i = 99; i >= 0; i--) {
+    console.log(i);
+}
+//3.
+for (var i = 0; i <= 98; i = i + 2) {
+    console.log(i); 
+}
+//4.
+for (var i = 98; i >= 0; i = i - 2) {
+    console.log(i); 
+}
+//7.
+for (var i = 49; i <= 72; i++) {
+    console.log(i); 
+}
