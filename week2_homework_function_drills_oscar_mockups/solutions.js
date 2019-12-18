dd6f73edfbe5335272716f74a6dd2f6cf96b4e55
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2019-12-18 14:27:23 -0800

    palindrome completed

---
+++ week2_homework_function_drills_oscar_mockups/solutions.js
@@ -0,0 +1,5 @@
+// Verbal Questions
+// 1. A parameter is the variable assigned in the declaration of a function while the argument is the data you want to pass into the method's parameters.
+// 2. "Return" is a statement that outputs a value back to where it was called while a "console.log" is a statement that allows for value inspection in order to debug code.
+
+//
