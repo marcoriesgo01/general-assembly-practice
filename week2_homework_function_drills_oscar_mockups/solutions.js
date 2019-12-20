23152e45a559a17f0460d465c634677bf298692c
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2019-12-20 15:35:42 -0800

    palindrome completed

--- week2_homework_function_drills_oscar_mockups/solutions.js
+++ week2_homework_function_drills_oscar_mockups/solutions.js
@@ -2,4 +2,17 @@
 // 1. A parameter is the variable assigned in the declaration of a function while the argument is the data you want to pass into the method's parameters.
 // 2. "Return" is a statement that outputs a value back to where it was called while a "console.log" is a statement that allows for value inspection in order to debug code.
 
-//
+// Palindrome
+const checkPalindrome = (string) => {
+   const reversedString = string.toLowerCase().split("").reverse().join("");
+   if (reversedString === string.toLowerCase()) {
+       return true;
+   } else {
+   return false;
+   }
+};
+console.log(checkPalindrome("hello world"));
+console.log(checkPalindrome("racecar"));
+
+
+
---
+++ week3_homework_landscaper/app.js
@@ -0,0 +1,11 @@
+let moneyEarned = 0;
+let daysWorked = 0;
+let tools = ""
+
+const gamePlay = {
+    tool: "teeth",
+    daysWorked: 0,
+    moneyEarned: 0
+};
+
+
