6bba98c875a8cf9c2fd46ddc67dcbd63ef0f7a49
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2019-12-20 16:25:38 -0800

    sum array completed

--- week2_homework_function_drills_oscar_mockups/solutions.js
+++ week2_homework_function_drills_oscar_mockups/solutions.js
@@ -14,5 +14,14 @@ const checkPalindrome = (string) => {
 console.log(checkPalindrome("hello world"));
 console.log(checkPalindrome("racecar"));
 
+// Sum Array
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+};
 
+console.log(sumArray([1, 2, 3, 4, 5, 6, 7]));
 
