522020bf4e32b49046e6b8693b004d7e94576d9e
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-01-15 12:35:26 -0800

    created our enemy pizza rat

--- week4_homework_Classes_and_Donut_Adventure/donut_adventure/adventure.js
+++ week4_homework_Classes_and_Donut_Adventure/donut_adventure/adventure.js
@@ -25,3 +25,28 @@ class Hero {
 };
 
 const dougie = new Hero("Dougie the Donut");
+
+// Create Our Enemy Class:
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10    
+        };
+        this.catchPhrases = ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer'];
+    }
+    talkSmack() {
+        console.log(this.name + " says " + this.catchPhrases[Math.floor(Math.random() * arr.length)(this.catchPhrases)]);
+    }
+    announceHealth() {
+        console.log(this.name + "'s current health is " + this.health);
+    }
+    fight() {
+        console.log("I am gonna flatten you like a slice of pepperoni!");
+    }
+};
+
+const pizzaRat = new Enemy("Enemy Pizza Rat");
