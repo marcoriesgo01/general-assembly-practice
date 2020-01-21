469348600c956247d90d203f74816db888a0a3a5
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-01-20 19:26:43 -0800

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- week5_d3_homework_jquery_magic/hogwarts/app.js
+++ week5_d3_homework_jquery_magic/hogwarts/app.js
@@ -61,4 +61,82 @@ $(() => {
     $jellyBeans.text("Bertie Bott's Every Flavor [Jelly] Beans");
     $ul.append($jellyBeans);
 
+    const $tableTitle = $('<h5>');
+    $tableTitle.text("Spring 2020");
+    $container.append($tableTitle);
+
+    const $classes = $('<table>');
+    $container.append($classes);
+
+    const $thead = $('<thead>');
+    $classes.append($thead);
+
+    const $day = $('<th>');
+    $day.text("Day");
+    $thead.append($day);
+    
+    const $class = $('<th>');
+    $class.text("Classes");
+    $thead.append($class);
+
+    const $tr = $('<tr>');
+    $classes.append($tr);
+
+    const $monday = $('<td>');
+    $monday.text("Monday");
+    $tr.append($monday);
+
+    const $mondayClass = $('<td>');
+    $mondayClass.text("Potions, Quidditch practice, Herbology");
+    $tr.append($mondayClass);
+
+    const $tr2 = $('<tr>');
+    $classes.append($tr2);
+
+    const $tuesday = $('<td>');
+    $tuesday.text("Tuesday");
+    $tr2.append($tuesday);
+
+    const $tuesdayClass = $('<td>');
+    $tuesdayClass.text("History of Magic, Defense Against the Dark Arts, Charms");
+    $tr2.append($tuesdayClass);
+
+    const $tr3 = $('<tr>');
+    $classes.append($tr3);
+
+    const $wednesday = $('<td>');
+    $wednesday.text("Wednesday");
+    $tr3.append($wednesday);
+
+    const $wednesdayClass = $('<td>');
+    $wednesdayClass.text("Divination, Transfiguration, Quidditch practice");
+    $tr3.append($wednesdayClass);
+
+    const $tr4 = $('<tr>');
+    $classes.append($tr4);
+
+    const $thursday = $('<td>');
+    $thursday.text("Thursday");
+    $tr4.append($thursday);
+
+    const $thursdayClass = $('<td>');
+    $thursdayClass.text("Charms, Potions, History of Magic");
+    $tr4.append($thursdayClass);
+
+    const $tr5 = $('<tr>');
+    $classes.append($tr5);
+
+    const $friday = $('<td>');
+    $friday.text("Friday");
+    $tr5.append($friday);
+
+    const $fridayClass = $('<td>');
+    $fridayClass.text("Defence Against the Dark Arts, Herbology, Divination");
+    $tr5.append($fridayClass);
+
+
+
+
+
+
 });
