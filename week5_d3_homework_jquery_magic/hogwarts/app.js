f4205a7a685188da7d00a531ab69683ed42579da
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-01-20 18:21:07 -0800

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- week5_d3_homework_jquery_magic/hogwarts/app.js
+++ week5_d3_homework_jquery_magic/hogwarts/app.js
@@ -7,5 +7,26 @@ $(() => {
     $h1.text('Hogwarts')
     $container.append($h1);
 
+    const $h2 = $('<h2>');
+    console.log($h2);
+    $h2.text('Marco Riesgo');
+    $container.append($h2);
+
+    const $h3 = $('<h3>');
+    console.log($h3);
+    $h3.text('Gryffindor');
+    $container.append($h3);
+
+    const $h4pet = $('<h4>');
+    $h4pet.addClass('toad');
+    console.log($h4pet);
+    $h4pet.text('Phillip the Toad');
+    $container.append($h4pet);
+
+    const $h4wand = $('<h4>');
+    console.log($h4wand);
+    $h4wand.text('The Elder Wand');
+    $container.append($h4wand);
+    
 
 });
