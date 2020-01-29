683c24ce184814ca90e42f557e10c0b4c2052802
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-01-29 12:39:27 -0800

    Chapter 2 complete - Made the Hobbits

--- week6_d2_homework_LOTR_lite/fellowship.js
+++ week6_d2_homework_LOTR_lite/fellowship.js
@@ -69,19 +69,21 @@ const makeMiddleEarth = () => {
 // Chapter 2
 // ============
 const makeHobbits = () => {
-
+    console.log("Trying to make hobbits");
   // Goal: display an unordered list of hobbits in the shire (which is the first article tag on the page)
-
   // 1. create a 'ul'
-
+  let $ul = $("<ul>");
   // 2. make each hobbit an li element and append it to the 'ul' you just created
     // hint: use the given 'hobbits' array and use a for loop
-
+    for (let i=0; i< hobbits.length; i++){
+        let $li = $("<li>").text(hobbits[i]);
   // 3. also, give each hobbit (`li`) a class of "hobbit"
-
+  $li.addClass("hobbit");
+  $ul.append($li);
   // 4. append the ul to the shire
     // hint: get 'The-Shire' by using its id
-
+    $("#The-Shire").append($ul);
+    }
 };
 
 // COMMIT YOUR WORK
