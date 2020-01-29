72769c30266d0248b1e65984b5c9c13fb6bece86
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-01-29 12:58:15 -0800

    Chapter 6 complete - Left the Shire

--- week6_d2_homework_LOTR_lite/fellowship.js
+++ week6_d2_homework_LOTR_lite/fellowship.js
@@ -156,11 +156,12 @@ const makeBuddies = () => {
 // ============
 
 const leaveTheShire = () => {
+    console.log("Trying to leave the shire");
   // 1. grab the hobbits (the ul in which they reside) and move them to Rivendell
       // hint: the hobbits ul is a childNode of The-Shire-- there is way to get a list of childNodes
-
-  //Stretch: add an event handler/listener so that when you click on the `h1` The Shire, this function will be called (be sure to do it in the window.onload/document.ready function)
-
+    let $movedHobbits = $("#The-Shire").children("ul");
+    $("#Rivendell").append($movedHobbits);
+    $("#The-Shire").remove("ul");
 };
 
 // COMMIT YOUR WORK
