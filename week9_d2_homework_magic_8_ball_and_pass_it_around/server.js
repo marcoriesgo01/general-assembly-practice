387b0554e5037a93534b89fcaa018bd899de4334
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-10 16:04:45 -0800

    Tip Calculator express application created.

--- week9_d2_homework_magic_8_ball_and_pass_it_around/server.js
+++ week9_d2_homework_magic_8_ball_and_pass_it_around/server.js
@@ -1,11 +1,18 @@
+// Begin simple backend using express:
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
+// Greetings:
 app.get("/greeting/:name", (req, res) => {
     res.send("Wow! Hey there " + req.params.name);
+});
+
+// Tip calculator:
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send("Your tip should be " + req.params.total * req.params.tipPercentage/100);
 });
 
 app.listen(port, () => {
