38a3577a99070e309e5d86fa92a25d833a4d714f
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-10 17:15:34 -0800

    Magic 8 Ball express application created.

--- week9_d2_homework_magic_8_ball_and_pass_it_around/server.js
+++ week9_d2_homework_magic_8_ball_and_pass_it_around/server.js
@@ -15,6 +15,43 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send("Your tip should be " + (req.params.total * req.params.tipPercentage/100));
 });
 
+// Magic 8 Ball:
+app.get("/magic/:question", (req, res) => {
+    const ballOptions = ["It is certain", 
+                        "It is decidedly so", 
+                        "Without a doubt", 
+                        "Yes definitely",
+                        "You may rely on it", 
+                        "As I see it yes", 
+                        "Most likely", 
+                        "Outlook good",
+                        "Yes", 
+                        "Signs point to yes", 
+                        "Reply hazy try again", 
+                        "Ask again later",
+                        "Better not tell you now", 
+                        "Cannot predict now", 
+                        "Concentrate and ask again",
+                        "Don't count on it", 
+                        "My reply is no", 
+                        "My sources say no",
+                        "Outlook not so good", 
+                        "Very doubtful"];
+    const randomization = Math.floor(Math.random() * ballOptions.length);
+    const ballResponse = ballOptions[randomization];
+    res.send(`<html>
+                    <body>
+                        <h3 style="text-align: center; font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">
+                            ${req.params.question}?
+                        </h3>
+                        <h1 style="text-align: center; font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">
+                            ${ballResponse}!
+                        </h1>
+                    <body>
+            </html>`);
+});
+
+
 app.listen(port, () => {
     console.log("I am listening for requests on port", port);
   });
