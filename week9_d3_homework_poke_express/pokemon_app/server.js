d78149ae4d63b52516239107dd3157a77b74a18d
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-27 14:44:11 -0800

    Connected my database, I can see JSON in the browser

--- week9_d3_homework_poke_express/pokemon_app/server.js
+++ week9_d3_homework_poke_express/pokemon_app/server.js
@@ -1,16 +1,26 @@
+//Start the app
 const express = require('express');
 const app = express();
 const port = 3000;
 
+//Connect the server to the database
+const pokemon = require('./models/pokemon.js')
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to the Pokemon App!');
+//Creating the pokemon route (basic):
+app.get('/pokemon', (req, res)=>{
+    res.send(pokemon);
 });
 
 
 
 
+//Basic port 3000 call:
+app.get('/', (req, res)=>{
+    res.send('Welcome to the Pokemon App!');
+});
 
+
+//Make the app listen:
 app.listen(port, ()=>{
     console.log('App running on port:' + port);
 });
