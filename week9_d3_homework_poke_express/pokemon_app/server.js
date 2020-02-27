961dfa21a97134620261e298d4a6387c2cfe0511
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-27 15:50:37 -0800

    Show view shows req.params.id

--- week9_d3_homework_poke_express/pokemon_app/server.js
+++ week9_d3_homework_poke_express/pokemon_app/server.js
@@ -6,17 +6,17 @@ const port = 3000;
 //Connect the server to the database:
 const pokemon = require('./models/pokemon.js')
 
-//Creating the pokemon route (index view):
+//Creating the pokemon route (index view to show the pokemon):
 app.get('/pokemon', (req, res)=>{
     res.render('index.ejs',
     {pokemon: pokemon});
 });
 
-//Setting up the index View to show pokemon data:
+//Creating a show route:
 
-
-
-
+app.get('/pokemon/:id', (req, res)=>{
+    res.send(req.params.id);
+});
 
 
 //Basic port 3000 call:
