b560d33b7572cd4e22d958c7613f20606048bf7a
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-27 20:58:53 -0800

    CSS Linked, routes started

--- week10_d2_homework_budgtr/server.js
+++ week10_d2_homework_budgtr/server.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Connect to the database:
+const budget = require('./models/budget.js');
 
+//Set up the middleware:
+app.use((req, res, next) => {
+    console.log("I run for all routes");
+    next();
+});
 
+app.use(express.urlencoded({extended: false}));
 
 
 
 
 //Basic port 3000 call:
 app.get('/', (req, res)=>{
-    res.send('Welcome to Budgtr!');
+    res.render('index.ejs', 
+    {budget: budget});
 });
 
 
