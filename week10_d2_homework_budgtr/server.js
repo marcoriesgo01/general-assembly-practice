4117145d838485bd365dd45094f7e459ea09f124
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-02-27 23:04:55 -0800

    completed the New section

--- week10_d2_homework_budgtr/server.js
+++ week10_d2_homework_budgtr/server.js
@@ -6,14 +6,20 @@ const port = 3000;
 //Connect to the database:
 const Budget = require('./models/budget.js');
 
+//initiate middleware:
+app.use(express.urlencoded({extended: false}));
+
 //Set up the middleware:
 app.use((req, res, next) => {
     console.log("I run for all routes");
     next();
 });
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+//Begin a practice route to check the app is working:
+app.get('/', (req, res) => {
+    res.send("App is connected");
+});
+
 
 //Index:
 app.get('/Budget', (req, res)=>{
@@ -21,14 +27,24 @@ app.get('/Budget', (req, res)=>{
     {Budget: Budget});
 });
 
+
+//Route to the new item page:
+//For some reason, the "add new item will not work if the Budget/new section is after the show route"
+app.get('/Budget/new', (req, res) =>{
+    res.render('new.ejs')
+});
+
+//Create a route in order to post a new expense to the list:
+app.post('/Budget', (req, res)=>{
+    Budget.push(req.body);
+    res.redirect('/Budget');
+});
+
 // Show route:
 app.get('/Budget/:id', (req, res)=>{
     res.render('show.ejs', 
     {Budget: Budget[req.params.id]});
 });
-
-
-
 
 //Make the app listen:
 app.listen(port, ()=>{
