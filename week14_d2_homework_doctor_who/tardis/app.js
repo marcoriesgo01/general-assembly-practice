da442f4a22e3c0fa1937200b05202824fd495d75
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-21 11:26:06 -0700

    Doctor Who's Police Box Completed

--- week14_d2_homework_doctor_who/tardis/app.js
+++ week14_d2_homework_doctor_who/tardis/app.js
@@ -1,45 +1,119 @@
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tardis: {
-                name: 'Time and Relative Dimension in Space',
-                caps: false,
-            }
-        }
-    }
+class DivOne extends React.Component {
+  render() {
+    return (
+      <div>
+        <DivTwo
+          changeIt={this.props.changeIt}
+          tardis={this.props.tardis}
+          changeSecond={this.props.changeSecond}
+          second={this.props.second}
+        />
+      </div>
+    );
+  }
+}
 
-    changeIt = (text) => {
-        console.log("Clicked");
-        if (this.state.tardis.caps) {
-          this.setState({
-            tardis: {
-              name: text.toLowerCase(),
-              caps: false
-            }
-          })
-        } else {
-          this.setState({
-            tardis: {
-              name: text.toUpperCase(),
-              caps: true
-            }
-          })
-        }
-        
-      }
+class DivTwo extends React.Component {
+  render() {
+    return (
+      <div>
+        <DivThree changeIt={this.props.changeIt} tardis={this.props.tardis} />
+        <DivFour
+          changeSecond={this.props.changeSecond}
+          second={this.props.second}
+        />
+      </div>
+    );
+  }
+}
 
-    render () {
-        return (
-            <div onClick={this.changeIt}>
-                {this.state.tardis.name}
-            </div>
-        )
+class DivThree extends React.Component {
+  render() {
+    return (
+      <div className="topTextContainer">
+        <h3 onClick={this.props.changeIt}> {this.props.tardis.name} </h3>
+      </div>
+    );
+  }
+}
+
+class DivFour extends React.Component {
+  render() {
+    return (
+      <div className="bottomTextContainer">
+        <h3 onClick={this.props.changeSecond}> {this.props.second.name} </h3>
+      </div>
+    );
+  }
+}
+
+ class App extends React.Component {
+  state = {
+    tardis: {
+      name: "Time and Relative Dimension in Space",
+      caps: false
+    },
+    second: {
+      name: "Time and Relative Dimension in Space",
+      caps: false
     }
+  };
+
+  changeIt = () => {
+    let tardis = this.state.tardis.name;
+    if (this.state.tardis.caps) {
+      this.setState({
+        tardis: {
+          name: tardis.toLowerCase(),
+          caps: false
+        }
+      });
+    } else {
+      this.setState({
+        tardis: {
+          name: tardis.toUpperCase(),
+          caps: true
+        }
+      });
+    }
+  };
+
+  changeSecond = () => {
+    let second = this.state.second.name;
+    if (this.state.second.caps) {
+      this.setState({
+        second: {
+          name: second.toLowerCase(),
+          caps: false
+        }
+      });
+    } else {
+      this.setState({
+        second: {
+          name: second.toUpperCase(),
+          caps: true
+        }
+      });
+    }
+  };
+
+  render() {
+    console.log(this.state.second.name);
+    return (
+      <div>
+        <DivOne
+          changeIt={this.changeIt}
+          tardis={this.state.tardis}
+          changeSecond={this.changeSecond}
+          second={this.state.second}
+        />
+      </div>
+    );
+  }
 }
 
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('.container')
+  <App />,
+  document.querySelector('.container')
 )
