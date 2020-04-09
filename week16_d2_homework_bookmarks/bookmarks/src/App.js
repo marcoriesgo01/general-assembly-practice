6b919a1907fae088a6ad9a14766b7901281dbc95
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-09 15:47:30 -0700

    Bookmarks App complete - working on styling

--- week16_d2_homework_bookmarks/bookmarks/src/App.js
+++ week16_d2_homework_bookmarks/bookmarks/src/App.js
@@ -60,7 +60,7 @@ class NewBookmarkForm extends React.Component {
   render() {
     return(
       <div className="form-container">
-        <h2>Add A New Bookmark</h2>
+        <h3>Add A New Bookmark</h3>
         <form onSubmit={this.handleSubmit}>
           <div class="form-group">
             <label htmlFor="website-name">Website name</label>
@@ -70,7 +70,7 @@ class NewBookmarkForm extends React.Component {
             <label htmlFor="website-url">Website URL</label>
             <input type="text" class="form-control" id="url" name="url" onChange={this.handleChange} placeholder="URL" />
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-outline-primary" id="add-bookmark-button">Submit</button>
         </form>
       </div>
     );
@@ -80,13 +80,68 @@ class NewBookmarkForm extends React.Component {
 
 
 
+class EditBookmarkForm extends React.Component {
+
+  state = {
+    title: '',
+    url: ''
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    fetch(this.props.baseURL + "/bookmarks/" + this.props.bookmark._id, {
+      method: "PUT",
+      body: JSON.stringify({ 
+          title: this.state.title,
+          url: this.state.url
+          }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(resJson => {
+      this.props.editBookmark(resJson);
+    })
+    .then(event => {
+      this.props.closeEditForm(event)
+    })
+    .catch(error => console.error({ Error: error }));
+  }
+
+  render() {
+    return(
+      <div className="form-container">
+        <h2>Edit Your {this.props.bookmark.title} Bookmark</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div class="form-group">
+            <label htmlFor="website-name">Website name</label>
+            <input type="text" class="form-control" id="title" name="title" onChange={this.handleChange} placeholder={this.props.bookmark.title} />
+          </div>
+          <div class="form-group">
+            <label htmlFor="website-url">Website URL</label>
+            <input type="text" class="form-control" id="url" name="url" onChange={this.handleChange} placeholder={this.props.bookmark.url} />
+          </div>
+          <button type="submit" class="btn btn-outline-primary" id="submit-edit-button">Submit Edit</button>
+        </form>
+        <button type="button" onClick={(event) => this.props.closeEditForm(event)} class="btn btn-outline-secondary" id="cancel-edit-button">Cancel</button>
+      </div>
+    );
+  }
+}
+
 
 
 
 class App extends React.Component {
 
   state = {
-    bookmarks: []
+    bookmarks: [],
+    addBookmarkForm: true
   }
 
   getBookmarks = () => {
@@ -128,28 +183,56 @@ class App extends React.Component {
     })
   }
 
+  getBookmark = bookmark => {
+    this.setState({
+      bookmark,
+      editBookmarkForm: true,
+      addBookmarkForm: false
+    })
+  }
+
+
+  editBookmark = (resJson) => {
+    const copyBookmarks = [...this.state.bookmarks]
+    const findIndex = this.state.bookmarks.findIndex(bookmark => bookmark._id === bookmark._id)
+    copyBookmarks[findIndex].title = resJson.title
+    copyBookmarks[findIndex].url = resJson.url
+    this.setState({bookmarks: copyBookmarks})
+  }
+
+  handleEditBookmarkSubmit = () => {
+    this.setState({
+      editBookmarkForm: false,
+      addBookmarkForm: true
+    });
+  }
+
 
   render() {
     return (
       <div className="app">
         <div className="navBar">
-          <h1>Bookmarks</h1>
+          <h1>Bookmark'd</h1>
         </div>
-        <NewBookmarkForm baseURL={baseURL} handleAddBookmark={this.handleAddBookmark} />
+        <div className="container">
+        { this.state.addBookmarkForm ? <NewBookmarkForm baseURL={baseURL} handleAddBookmark={this.handleAddBookmark} /> : null }
+        { this.state.editBookmarkForm ? <EditBookmarkForm baseURL={baseURL} bookmark={this.state.bookmark} closeEditForm={this.handleEditBookmarkSubmit} editBookmark={this.editBookmark} /> : null }
         <div className="bookmarks-container">
-          <table class="table table-bordered">
+          <table class="table table-hover table-bordered">
             <thead>
               <tr>
-                <th scope="col">Bookmarks List</th>
+                <th scope="col"><h3>Bookmarks List</h3></th>
               </tr>
             </thead>
             <tbody>
             {this.state.bookmarks.map(bookmark => (
               <tr key={bookmark._id}>
                 <td>
-                  <a href={bookmark.url} target="_blank">{bookmark.title}</a> 
-                  <button type="button" onClick={() => this.deleteBookmark(bookmark._id)} class="btn btn-outline-info" id="edit-bookmark-button">Edit</button>
-                  <button type="button" onClick={() => this.deleteBookmark(bookmark._id)} class="btn btn-outline-danger" id="delete-bookmark-button">Delete</button>
+                  <a href={bookmark.url} target="_blank"><h5>{bookmark.title}</h5></a>
+                  <div className="list-buttons-container">
+                    <button type="button" onClick={() => this.getBookmark(bookmark)} class="btn btn-outline-info btn-sm" id="edit-bookmark-button">Edit</button>
+                    <button type="button" onClick={() => this.deleteBookmark(bookmark._id)} class="btn btn-outline-danger btn-sm" id="delete-bookmark-button">Delete</button>
+                  </div>
                 </td>
               </tr>
               )
@@ -158,6 +241,7 @@ class App extends React.Component {
           </table>
         </div>
       </div>
+    </div>
     );
   }
   componentDidMount() {
