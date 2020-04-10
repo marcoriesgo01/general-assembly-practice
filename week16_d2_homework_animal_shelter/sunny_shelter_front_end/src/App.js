ed1dfdbef01f17acd37986f7dbaaf8ec49a66ac0
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-09 18:59:56 -0700

    working on animal shelter

--- week16_d2_homework_animal_shelter/sunny_shelter_front_end/src/App.js
+++ week16_d2_homework_animal_shelter/sunny_shelter_front_end/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import NewAnimalForm from "./Components/NewAnimalForm.js";
+// import NewAnimalForm from "./Components/NewAnimalForm.js";
 
 let baseURL = process.env.REACT_APP_BASEURL;
 
@@ -21,6 +21,84 @@ fetch(baseURL + "/animals")
     parsedData => console.log(parsedData),
     err => console.log(err)
 );
+
+
+class NewAnimalForm extends React.Component {
+
+  state = {
+    name: '',
+    species: '',
+    breed: '',
+    image: '',
+    age: 0,
+    adopted: false,
+    personalityTraits: []
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    fetch(this.props.baseURL + "/animals", {
+      method: "POST",
+      body: JSON.stringify({ 
+          name: this.state.name,
+          species: this.state.species,
+          breed: this.state.breed,
+          image: this.state.image,
+          age: this.state.age
+         }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(resJson => {
+      this.props.handleAddAnimal(resJson);
+      this.setState({
+        name: '',
+        species: '',
+        breed: '',
+        image: '',
+        age: ''
+      });
+    })
+    .catch(error => console.error({ Error: error }));
+  }
+
+  render() {
+    return(
+      <div className="form-container">
+        <h3>Add A New Animal</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div class="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" onChange={this.handleChange} placeholder="Name" />
+          </div>
+          <div class="form-group">
+            <label htmlFor="species">Species</label>
+            <input type="text" class="form-control" id="species" name="species" onChange={this.handleChange} placeholder="Species" />
+          </div>
+          <div class="form-group">
+            <label htmlFor="breed">Breed</label>
+            <input type="text" class="form-control" id="breed" name="breed" onChange={this.handleChange} placeholder="Breed" />
+          </div>
+          <div class="form-group">
+            <label htmlFor="image">Image URL</label>
+            <input type="text" class="form-control" id="image" name="image" onChange={this.handleChange} placeholder="Image URL" />
+          </div>
+          <div class="form-group">
+            <label htmlFor="age">Age</label>
+            <input type="text" class="form-control" id="age" name="age" onChange={this.handleChange} placeholder="Age" />
+          </div>
+          <button type="submit" class="btn btn-outline-primary" id="add-bookmark-button">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 
 class App extends Component {
@@ -51,7 +129,7 @@ class App extends Component {
       species: '',
       breed: '',
       image: '',
-      age: Number,
+      age: '',
       animal: {}
     });
   };
@@ -86,21 +164,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
+        <div className="navBar">
           <h1>Animal Shelter</h1>
-        </header>
-        <div className="App">
-          <h2>Add A New Animal</h2>
+        </div>
+        <div className="form-app-container">
           <NewAnimalForm baseURL={baseURL} handleAddAnimal={this.handleAddAnimal} />
-          <table class="table table-striped">
-            <thead class="thead-dark">
+        </div>
+        <div className="animals-container">
+          <table class="table table-bordered table-hover">
+            <thead>
               <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Species</th>
-                <th scope="col">Breed</th>
-                <th scope="col">Image</th>
-                <th scope="col">Age</th>
-                <th scope="col">Adoption Status</th>
+                <th scope="col"><h5>Name</h5></th>
+                <th scope="col"><h5>Species</h5></th>
+                <th scope="col"><h5>Breed</h5></th>
+                <th scope="col"><h5>Image</h5></th>
+                <th scope="col"><h5>Age</h5></th>
+                <th scope="col"><h5>Status</h5></th>
+                <th scope="col"><h5>Options</h5></th>
               </tr>
             </thead>
             <tbody>
@@ -109,11 +189,10 @@ class App extends Component {
                   <td>{animal.name}</td>
                   <td>{animal.species}</td>
                   <td>{animal.breed}</td>
-                  <td>{animal.image}</td>
+                  <td className="button-td"><img src={animal.image} alt="Animal Image"></img></td>
                   <td>{animal.age}</td>
                   <td>{animal.adopted}</td>
-                  <td onDoubleClick={() => this.toggleAdopted(animal)}>{animal.adopted}</td>
-                  <td onClick={() => this.deleteAnimal(animal._id)}>X</td>
+                  <td className="button-td"><button type="button" onClick={() => this.deleteAnimal(animal._id)} class="btn btn-outline-danger btn-sm" id="animal-list-button">Remove</button></td>
                 </tr>
                 )
               )}
