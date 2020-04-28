ef47a368401acd6c696624af25826ff3b111ba12
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-28 13:06:19 -0700

    Ruby Jackblack completed

--- week18_d3_homework_ruby_black_jack/blackjack.rb
+++ week18_d3_homework_ruby_black_jack/blackjack.rb
@@ -1,47 +1,133 @@
-# Players:
-
+# Player Class
 class Player
-    attr_accessor :name, :bankroll, :hand
-    def initialize name, bankroll, hand
-        @name = ""
-        @bankroll = 100
-        @hand = []
-    end
+	attr_accessor :name, :hand, :bankroll, :total
+	def initialize name, hand, bankroll, total
+		@name = name
+		@hand = []
+		@bankroll = bankroll
+		@total = total
+	end
 end
 
-#Create the class of computer to play against the player
-class Computer
-    attr_accessor :name, :bankroll, :hand
-    def initialize name, bankroll, hand
-        @name = ""
-        @bankroll = 10000
-        @hand = []
-    end
+# Create Player and Computer Players
+human = Player.new "Human", [], 100, 0
+computer = Player.new "The House", [], 10000, 0
+
+
+# Deck of 52 playing cards
+class Deck
+	def initialize
+		#create an array of face values, suits and an empty array for cards
+		@faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']
+		@suits = ['clubs', 'spades', 'hearts', 'diamonds']
+		@cards = []
+        #Create each card based on face and suit
+		@faces.each do |face|
+			if face.class == Integer
+				value = face
+			elsif face == 'Ace'
+				value = 11
+			else
+				value = 10
+			end
+			@suits.each do |suit|
+				@cards << Card.new(value)
+			end
+		end
+	# Shuffle the cards
+	@cards.shuffle!
+	end
+
+	# deal the players their cards
+	def deal (num, player)
+		# shift the deck each time a card is dealt to a player
+		num.times {@cards.shift.generate_card(player)}
+	end
 end
 
-# The Deck:
-@deck = []
-
-#Create a class of card to create new cards that will be added to the deck array above.
+# Card class
 class Card
-    attr_accessor :value
-    def initialize value
-        @value = value
-    end
+	attr_accessor :value
+	def initialize value
+		@value = value
+	end
+
+	# generates a new card from the deck
+	# takes a parameter "player" and adds to that player's hand
+	def generate_card (player)
+		new_card = Card.new value
+		player.hand << new_card
+		# measure the value of the players current hand
+		player.total = player.total + new_card.value
+	end
 end
 
-# Set the values: Each number with corresponding point value to be assigned. Jack, King, Queen have value of 10. Ace is 11. 
-deck_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-4.times do 
-    deck_values.each do |val|
-        @deck << Card.new(val)
-    end
-end
+# Get Player name
+puts "Welcome to the casino. What is your name?"
+name = gets.chomp
+puts "\nWelcome #{name}."
+human.name = "#{name}"
 
-# Check if the deck has been created correctly to have 52 cards
-p @deck.length
+# Start Game Loop
+loop do
+	puts "(d)eal me or (q)uit table"
+	deal_me = gets.chomp.downcase
+		# If player quits table, end loop
+		if deal_me == "q"
+			puts "See you soon."
+			break
+		else
+			# reset player hands
+			human.hand.clear
+			human.total = 0
+			computer.hand.clear
+			computer.total = 0
 
-
-
-
-
+			# deal player cards
+			deck = Deck.new
+            deck.deal(2, human)
+            deck.deal(2, computer)
+            puts "The score of your cards is #{human.total}"
+            puts "The score of The House is #{computer.total}"
+			
+			# Check the score
+			if human.total <= 21 && computer.total <= 21
+				if human.total > computer.total
+					puts "You win!"
+					human.bankroll += 10
+					computer.bankroll -= 10
+				elsif human.total < computer.total
+					puts "Computer wins."
+					human.bankroll -= 10
+					computer.bankroll += 10
+				else
+					puts "It's a tie. Computer Wins."
+					human.bankroll -= 10
+					computer.bankroll += 10
+				end
+			# if human is above 21, human loses
+			elsif human.total > 21 && computer.total <= 21
+				puts "You went over 21. Computer wins."
+				human.bankroll -= 10
+				computer.bankroll += 10
+			# if computer is above 21, human wins
+			elsif human.total <= 21 && computer.total > 21
+				puts "Computer went over 21. You win!"
+				human.bankroll += 10
+				computer.bankroll -= 10
+			else
+				# if both are above 21, no one wins.
+				puts "Both hands went over 21. Nobody wins this round."
+			end
+			# Tell bankroll at all times
+			puts "You currently have $#{human.bankroll}"
+			puts "The House has $#{computer.bankroll}\n\n"
+				if human.bankroll <= 0
+					puts "You have no more money. You lost."
+					break
+				else
+				# Prompt to play again
+				puts "Play again?"
+				end
+		end
+	end
---
+++ week18_d3_homework_ruby_black_jack/test.rb
@@ -0,0 +1,182 @@
+class Human
+    #add attr_accessor for read and write
+    #for the properties below
+    attr_accessor :name
+    attr_accessor :bankroll
+    attr_accessor :hand
+    #initiantiate a hash of the class by
+    #setting the initalize function in the class
+    def initialize
+        #@variable_name allows the variable to be 
+        #accessible outside this function
+        @name = ""
+        @bankroll = 100
+        @hand = []
+    end
+
+end
+
+##class for Computer player
+class Computer 
+    attr_accessor :name
+    attr_accessor :bankroll
+    attr_accessor :hand
+
+    def initialize
+        @name = "the_house"
+        @bankroll = 10000
+        @hand = []
+    end
+end
+
+
+@deck = []
+@suits = ["hearts", "diamonds", "spades", "clubs"]
+
+#Class for cards
+class Card
+    attr_reader :value
+    attr_reader :face
+    attr_reader :suit
+
+    def initialize value, face, suit
+        @value = value
+        @face = face
+        @suit = suit
+    end
+end
+
+##Ruby method to populate the deck 
+def create_populate suit
+    deck = []
+    faces = [["Jack", 10],["Queen", 10],["King", 10],["Ace", 11]]
+    (2..10).each do |num| 
+        card = Card.new num, num, suit
+        deck.push(card)
+    end
+
+    faces.each do |face|
+        card = Card.new face[1], face[0], suit
+        deck.push(card)
+    end
+
+    deck
+end
+
+@suits.each do |suit|
+@deck.push(create_populate suit)
+end
+
+@deck = @deck.flatten
+#shuffle deck
+@deck = @deck.shuffle!
+
+#instantiate the computer player
+@the_house = Computer.new
+# p @the_house
+
+##instantiate the human player
+@human_player = Human.new 
+#####Game Logic
+
+def compare_sum human, computer
+    p "Do you want to bet the (m)inimum $10 or (i)ncrease your bet?"
+    answer = gets.chomp
+    if answer == "m"
+        @bet = 10
+    elsif answer == "i"
+        p "How much do you want to bet?"
+        @bet = gets.chomp.to_i
+    end
+    human_score = human.hand[0].value + human.hand[1].value
+    computer_score = computer.hand[0].value + computer.hand[1].value
+    p "#{human.name}'s score: #{human_score}"
+    p "#{computer.name}'s score: #{computer_score}"
+    if (human_score <= 21 && computer_score <= 21)
+        if human_score == computer_score
+            p "There is a tie!"
+        elsif human_score == 21
+            human.bankroll += @bet
+            computer.bankroll -= @bet
+            p "#{human.name} wins the round"
+        elsif computer_score == 21
+            computer.bankroll += @bet
+            human.bankroll -= @bet
+            p "#{computer.name} wins the round"
+        elsif human_score > computer_score
+            human.bankroll += @bet
+            computer.bankroll -= @bet
+            p "#{human.name} wins the round"
+        elsif
+            human_score < computer_score
+            computer.bankroll += @bet
+            human.bankroll -= @bet
+            p "#{computer.name} wins the round"
+        end
+    elsif human_score > 21
+        computer.bankroll += @bet
+        human.bankroll -= @bet
+        p "#{computer.name} wins the round"
+    elsif computer_score > 21
+        human.bankroll += @bet
+        computer.bankroll -= @bet
+        p "#{human.name} wins the round"
+    end
+        
+end
+def black_jack_round
+    # p @deck.length
+    if @deck.length > 0
+        if @human_player.name == ""
+            p "What's your name?"
+            name = gets.chomp
+            @human_player.name = name
+        end
+        p "#{@human_player.name} you currently have $#{@human_player.bankroll}"
+        p "#{@the_house.name} currently has $#{@the_house.bankroll}"
+        p "You will receive two random cards"
+        @human_player.hand[0] = @deck.sample
+        @deck.delete(@human_player.hand[0])
+        @human_player.hand[1] = @deck.sample
+        @deck.delete(@human_player.hand[1])
+        @the_house.hand[0] = @deck.sample
+        @deck.delete(@the_house.hand[0])
+        @the_house.hand[1] = @deck.sample
+        @deck.delete(@the_house.hand[1])
+        p "#{@human_player.name} has a #{@human_player.hand[0].face} of #{@human_player.hand[0].suit} and a #{@human_player.hand[1].face} of #{@human_player.hand[1].suit}"
+        p "#{@the_house.name} has a #{@the_house.hand[0].face} of #{@the_house.hand[0].suit} and a #{@the_house.hand[1].face} of #{@the_house.hand[1].suit}"
+
+        compare_sum @human_player, @the_house
+
+         game_loop
+
+    end
+end
+
+@round_counter = 0
+
+def game_loop
+    if @deck.length > 0 && @human_player.bankroll > 0
+        p "This is Ruby Black Jack"
+        p "(d)eal or (q)uit or check (b)ankroll"
+        answer = gets.chomp
+        if answer == "d"
+            @round_counter += 1
+            p "This is Round #{@round_counter}"
+            black_jack_round
+        elsif answer == "q"
+            p "The game has ended"
+        elsif answer == "b"
+            p "Player has $#{@human_player.bankroll}"
+            game_loop
+        end
+    elsif
+        if @deck.length == 0
+            p "No more cards. Game Over!"
+        elsif @human_player.bankroll == 0
+            p "You have $0 in your bank account. Game Over!"
+        end
+    end
+end
+
+game_loop
