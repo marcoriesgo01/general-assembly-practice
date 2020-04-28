ee021dcdae405d1764411798571fe577953f17ca
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-28 13:57:14 -0700

    Commit 9: Reset database

--- week19_d3_homework_glitch_in_the_matrix/matrix_app_api/db/seeds.rb
+++ week19_d3_homework_glitch_in_the_matrix/matrix_app_api/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "very smart" },
+	{ name: "Trinity", description: "she is a woman" },
+	{ name: "Morpheus", description: "a little overweight" },
+	{ name: "Agent Smith", description: "the bad guy" },
 ])
