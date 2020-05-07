e4735faebc82e58240aa2d04db6db342172a5541
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-07 15:24:24 -0700

    Commit 3: React module and controller

--- week20_d3_homework_rails_react_dating_app/dating-app-client/src/components/Hello.js
+++ week20_d3_homework_rails_react_dating_app/dating-app-client/src/components/Hello.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Daters extends Component {
-  render () {
-    return (
-      <h1>Hello Dating World</h1>
-    )
+
+    getDaters () {
+      fetch('http://localhost:3000/daters')
+      .then( daters => console.log(daters))
+    }
+    render () {
+      return (
+        <h1>Hello Dating World</h1>
+      )
+    }
   }
-}
 
 export default Daters;
