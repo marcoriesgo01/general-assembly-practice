fe9de65aa5c1aaf28f07a0bcaf687f5ccb661f3c
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-07 15:21:17 -0700

    Commit 2: Made React App

---
+++ week20_d3_homework_rails_react_dating_app/dating-app-client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Daters from './components/Hello.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Daters />
+      </div>
+    );
+  }
+}
+
+export default App;
