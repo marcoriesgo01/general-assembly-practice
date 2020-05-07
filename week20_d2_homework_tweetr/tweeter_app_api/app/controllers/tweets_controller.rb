a2a406d71a755d586c0111752cd609ed6a393643
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-07 14:43:53 -0700

    Commit 9: Tweets update

--- week20_d2_homework_tweetr/tweeter_app_api/app/controllers/tweets_controller.rb
+++ week20_d2_homework_tweetr/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -17,6 +17,12 @@ class TweetsController < ApplicationController
           render(status: 422, json: { tweet: tweet, errors: tweet.errors })
         end
     end
+
+    def update
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render(json: { tweet: tweet })
+    end
   
     private
   
