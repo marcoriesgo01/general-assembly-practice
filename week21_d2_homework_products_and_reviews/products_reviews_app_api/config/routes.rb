d98cd19c2a097190a172a114218573e35587bccf
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-10 22:12:17 -0700

    Commit 8: Routes changed for Reviews update and delete

--- week21_d2_homework_products_and_reviews/products_reviews_app_api/config/routes.rb
+++ week21_d2_homework_products_and_reviews/products_reviews_app_api/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
     resources :reviews, except: [:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :reviews, only: [:update, :destroy]
+
 end
