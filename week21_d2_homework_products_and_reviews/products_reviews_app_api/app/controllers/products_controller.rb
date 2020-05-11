ff64bfa4cc943c900cfd58678630697f68517234
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-10 21:56:37 -0700

    Commit 6: Products controller fixed

--- week21_d2_homework_products_and_reviews/products_reviews_app_api/app/controllers/products_controller.rb
+++ week21_d2_homework_products_and_reviews/products_reviews_app_api/app/controllers/products_controller.rb
@@ -1,41 +1,18 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  
 
   # GET /products
   def index
     @products = Product.all
 
-    render json: @products
+    render json: @products, status: 200
   end
 
   # GET /products/1
   def show
-    render json: @product
-  end
+    @product = Product.find(params[:id])
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
+    render json: @product, status: 200
   end
 
   private
@@ -44,8 +21,4 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def product_params
-      params.require(:product).permit(:name, :price, :img)
-    end
 end
